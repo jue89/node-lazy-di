126d23ac0dbd88631787e34f6caa4732e54f29ab
Juergen Fitschen <me@jue.yt>
2023-06-29 22:15:54 +0200

    feat(helper): provide optional and integer env loader

--- helper.mjs
+++ helper.mjs
@@ -9,6 +9,25 @@ export function addEnvLoader (lib) {
 			assert(env !== undefined, `Environment ${name} not set`);
 			return env;
 		}
+	}).add({
+		provides: 'env-optional::*',
+		factory: (dep, name) => {
+			const env = process.env[name];
+			return env;
+		}
+	}).add({
+		provides: 'envint::*',
+		factory: (dep, name) => {
+			const env = process.env[name];
+			assert(env !== undefined, `Environment ${name} not set`);
+			return parseInt(env);
+		}
+	}).add({
+		provides: 'envint-optional::*',
+		factory: (dep, name) => {
+			const env = process.env[name];
+			return parseInt(env);
+		}
 	});
 }
 
--- test/helper.mjs
+++ test/helper.mjs
@@ -7,9 +7,20 @@ import process from 'node:process';
 test('load envs', async () => {
 	const lib = new Library();
 	addEnvLoader(lib);
-	await assert.rejects(() => lib.get('env::NOT_SET'), /Environment NOT_SET not set/);
 	process.env['FOO'] = 'bar';
+	process.env['NUM'] = '42';
+
+	await assert.equal(await lib.get('env-optional::NOT_SET'), undefined);
+	await assert.equal(await lib.get('env-optional::FOO'), 'bar');
+
+	await assert.rejects(() => lib.get('env::NOT_SET'), /Environment NOT_SET not set/);
 	assert.equal(await lib.get('env::FOO'), 'bar');
+
+	await assert(isNaN(await lib.get('envint-optional::NOT_SET')));
+	await assert.equal(await lib.get('envint-optional::NUM'), 42);
+
+	await assert.rejects(() => lib.get('envint::NOT_SET'), /Environment NOT_SET not set/);
+	await assert.equal(await lib.get('envint::NUM'), 42);
 });
 
 test('load ext dependencies', async () => {
